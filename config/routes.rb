205ada0a295d52cb98c577e31d9988a13c1f4f5a
zzrr3321 <zzrr3321@gmail.com>
2018-03-13 11:42:25 +0800

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-
+	get '/signup', to: 'users#new'
+	post '/signup', to: 'users#create'
   # get '/home', 	to 'static_pages#home'
 
   get '/help', 	to: 'static_pages#help' 
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  resources :users
+
+
+
 end
